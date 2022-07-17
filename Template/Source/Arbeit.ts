c0ef8037c6f020227e742c60c211e5dc285d74fc
MauriceHuchler <maurice.huchler@hs-furtwangen.de>
2022-07-17 13:26:26 +0200

    stlying

---
+++ Template/Source/Arbeit.ts
@@ -0,0 +1,8 @@
+namespace Template {
+    export async function arbeit(): ƒS.SceneReturn {
+        await ƒS.Location.show(bg.bad);
+        await ƒS.update(0.5);
+
+        await ƒS.Speech.tell(char.Erzahler.name, "asdasd");
+    }
+}
--- Template/Source/Background.ts
+++ Template/Source/Background.ts
@@ -4,6 +4,14 @@ namespace Template {
             name: "schlafzimmer",
             background: "./Images/bg2.png",
             foreground: ""
+        },
+        bad: {
+            name: "bad",
+            background: "./Images/bg2.png"
+        },
+        arbeit: {
+            name: "arbeit",
+            background: "./Images/bg2.png"
         }
     }
 }
